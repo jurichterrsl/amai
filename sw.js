290447cf6e4e8b8298a697de9e7f2596c1fa0b5c
jurichter <jurichter@leggedrobotics.com>
2026-09-02 17:41:27 +0200

    bug fixing

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Amai service worker — App-Shell offline halten.
 // Bei jeder Änderung an den Dateien die Versionsnummer erhöhen.
-const CACHE = "amai-v14";
+const CACHE = "amai-v16";
 const SHELL = ["./", "./index.html", "./words.js", "./fsrs.js",
                "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
